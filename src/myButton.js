7932e4ccf078a742e27cdf4b1640948912c25d93
Viktoria <victoria.todorova0@gmail.com>
2023-10-02 18:58:35 +0200

    DELETE button is implemented through props

--- src/myButton.js
+++ src/myButton.js
@@ -3,10 +3,10 @@ import React from "react";
 import './my-button.css';
 import './input-and-button.css';
 
-export default function MyButton() {
+export default function MyButton(props) {
     return (
         <div>
-        <button className="my-button input-and-button" >
+        <button onClick={()=>{props.setValue("")}} className="my-button input-and-button"  >
        DEL
         </button>
         </div>
